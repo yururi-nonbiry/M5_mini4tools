de151d213386a9cba36dc066a1206760f1e89921
yururi <64812657+yururi-nonbiry@users.noreply.github.com>
2021-08-24 18:00:42 +0900

    Update mini4tools.py

--- test/mini4tools.py
+++ test/mini4tools.py
@@ -1,8 +1,10 @@
 from os import read
+import numpy as np
 import serial
 import configparser
 import time
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter
 import threading
 
@@ -38,21 +40,64 @@ class root():
         #ボタン関係
         self.connect_bt_text = tkinter.StringVar()
         self.connect_bt_text.set("接続")
-        self.connect_bt = tkinter.Button(self.root, textvariable=self.connect_bt_text, width=15)
-        self.connect_bt.place(x=105, y=60)
+        self.connect_bt = tkinter.Button(self.root, textvariable=self.connect_bt_text, width=5)
+        self.connect_bt.place(x=200, y=0)
         self.connect_bt.bind("<Button-1>",self.serial_start) 
 
-        self.calibration_bt = tkinter.Button(self.root,text="キャリブレーション", width=15)
+        self.calibration_bt = tkinter.Button(self.root,text="実行", width=5)
         self.calibration_bt.bind("<Button-1>", self.calibration)
-        self.calibration_bt.place(x=300, y=60)
+        self.calibration_bt.place(x=200, y=30)
         #self.buttonB.pack(side=tkinter.RIGHT)
 
-        # テキスト表示
+        # テキスト表示(静的)
+        self.calibration_info = tkinter.Label(self.root, text=u'シリアル接続')
+        self.calibration_info.place(x=10, y=0)
+        self.calibration_info = tkinter.Label(self.root, text=u'キャリブレーション')
+        self.calibration_info.place(x=10, y=30)
+
+        # テキスト表示(動的)
         self.calibration_label_text = tkinter.StringVar()
         self.calibration_label_text.set("未完了")
         self.calibration_label = tkinter.Label(self.root, textvariable=self.calibration_label_text)
-        self.calibration_label.place(x=100, y=300)
+        self.calibration_label.place(x=300, y=30)
 
+        # グラフ表示
+
+        x1 = np.linspace(0.0, 5.0)
+        y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
+        x2 = np.linspace(0.0, 3.0)
+        y2 = np.cos(2 * np.pi * x2) * np.exp(-x1)
+
+        self.fig = plt.figure()
+        # ax1
+        ax1 = self.fig.add_subplot(221)
+        ax1.plot(x1, y1)
+        ax1.set_title('line plot')
+        ax1.set_ylabel('Damped oscillation')
+
+        # ax2
+        ax2 = self.fig.add_subplot(222)
+        ax2.scatter(x1, y1, marker='o')
+        ax2.set_title('Scatter plot')
+
+        # ax3
+        ax3 = self.fig.add_subplot(223)
+        ax3.plot(x2, y2)
+        ax3.set_ylabel('Damped oscillation')
+        ax3.set_xlabel('time (s)')
+
+        # ax4
+        ax4 = self.fig.add_subplot(224)
+        ax4.scatter(x2, y2, marker='o')
+        ax4.set_xlabel('time (s)')
+        #self.fig, ax_current = plt.subplots(1, 1)
+        #plt.plot(buf)
+        #line, = ax.plot( buf, color='blue')
+        #plt.pause(0.01)
+        #line.remove()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)  # Generate canvas instance, Embedding fig in root
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack()
 
         #コンフィグ読取
         self.config_read()
@@ -97,12 +142,14 @@ class root():
         self.calibration_value = sum(buf)/len(buf)
         self.calibration_label_text.set(str(self.calibration_value))
         
-
+    # コンフィグ読込
     def config_read(self):
         global serial_port
         global serial_bps
         global raw_sample
         global raw_frequency
+        global current_gain
+        global current_offset
 
         config_ini = configparser.ConfigParser()
         config_ini.read(config_pass, encoding='utf-8')
@@ -112,6 +159,9 @@ class root():
         raw_config = config_ini['raw']
         raw_sample = raw_config.get('sample')
         raw_frequency = raw_config.get('sampling_frequency')
+        current_config = config_ini['current']
+        current_gain = current_config.get('gain')
+        current_offset = current_config.get('offset')
 
     def serial_read(self):
 
@@ -119,6 +169,8 @@ class root():
         global serial_bps
         global raw_sample
         global raw_frequency
+        global current_gain
+        global current_offset
 
         read_trig = 0 #sirial読み込み用トリガ
         buf_read = []
@@ -149,6 +201,7 @@ class root():
                             buf_culc.append((buf_read[i*2] << 8)  + buf_read[i*2+1])
 
                         print('start')
+
                         if self.serial_task_mode == 0: # パスモード
                             pass
                         elif self.serial_task_mode == 1: # キャリブレーション
@@ -156,45 +209,14 @@ class root():
                             self.calibration_task(buf_culc)
                             self.serial_task_mode = 0
 
+                        elif self.serial_task_mode == 2: # 各種表示
+                            cur_list = (buf_culc - self.calibration_value) * current_gain / 200 + current_offset # 電流計算
+
                         self.culc(buf_read)
                         read_trig = 0
 
                 time.sleep(0.1)
 
-
-"""
-def ticker_task():
-
-
-
-    #メニューバー
-    men = tkinter.Menu(root)
-    root.config(menu=men) 
-
-    #メニューに親メニュー（ファイル）を作成する 
-    menu_file = tkinter.Menu(root) 
-    men.add_cascade(label='ファイル', menu=menu_file) 
-
-    #親メニューに子メニュー（開く・閉じる）を追加する 
-    menu_file.add_command(label='開く', command=open_file) 
-    menu_file.add_separator() 
-    menu_file.add_command(label='閉じる', command=close_disp)
-
-    #ボタン
-    Button = tkinter.Button(text=u'接続', width=15)
-    Button.place(x=105, y=60)
-    Button.bind("<Button-1>",serial_read) 
-    #Button.pack()
-
-    # ラベルの作成
-    label = tkinter.Label(root, text="This is the Label.")
-
-    #ラベルの表示
-    label.grid()
-
-    root.mainloop()
-
-"""
 
 if __name__ == '__main__':
     #config_read()
